7b16bb7d557b1dab4cfe856475f35fa171f5cfde
Himansu <himansumaurya@gmail.com>
2025-01-02 19:56:12 +0530

    adding stun server

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,21 @@ const pcConfig = {
                 'stun:stun4.l.google.com:19302',
             ],
         },
+        {
+            urls: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+        },
+        {
+            urls: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+        },
+        {
+            urls: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+        },
     ],
 };
 
